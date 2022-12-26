5d990fad2e722a20e5fe129043ae9a7560527a3b
Rodrigo de Albuquerque Santos Lima <61851387+Rodrigo234fv@users.noreply.github.com>
2022-12-26 22:19:47 +0000

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+/* function showDivOnScroll(scrollDistance){
+	if (window.pageYOffset > scrollDistance){
+		document.getElementById("scroll-div").style.display = "block";
+	}else{
+		document.getElementById("scroll-div").style.display = "none";
+	}
+}
+window.onscroll = function(){
+	showDivOnScroll(500);
+} */
